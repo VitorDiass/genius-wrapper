aec70776e9dfcc4baef55c73d92b1eceabd250c6
VitorDiass <vitordias28@hotmail.com>
2022-07-27 10:57:21 +0100

    index

--- index.ts
+++ index.ts
@@ -1,22 +1,3 @@
 import Client from "./src/client";
 
-
-const client = new Client();
-client.setBearerToken("lQJp48VLcvV8Yoj7vHTg-Qb1gI6korwaP1i0EqwxgjDGyqFHj27aSkQhzJoeOCfh")
-
-
-const getArtist = async () => {
-
-    try {
-        //successful request
-        const artistRequest = await client.getArtistById('16775', { text_format: ['html']});
-        console.log(artistRequest);
-    } catch (error) {
-        //error on request
-        console.log(error)
-    }
-
-}
-
-
 export default Client;
